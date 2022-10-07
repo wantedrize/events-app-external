801228fc7d70b43641a89842174abb1f7af69585
wantedrize <eliezermelendezleon@gmail.com>
2022-10-07 18:05:38 +0000

    test change

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Team Cloud Cocktail Website</h1>"));
+        chai.assert.isTrue(res.text.includes("Cocktail"));
         return done();
       });
 
