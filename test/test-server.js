c195c1f5117f3103595d5a0708f1cd3e35d8f3d9
wantedrize <eliezermelendezleon@gmail.com>
2022-10-07 17:59:39 +0000

    modified assertion

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Team Cloud Cocktail Website</h1>"));
         return done();
       });
 
